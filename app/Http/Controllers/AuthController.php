2070b7c0c9907ca02df7abafa0a7eca02721f474
lemon-sazu <lemon.sazu@gmail.com>
2021-04-16 19:27:01 +0600

    User registration api created and tested with postman

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+
+    public function registration(Request $request){
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('name'))
+
+        ]);
+    }
+
+    public function user(){
+        return "working";
+    }
 }
